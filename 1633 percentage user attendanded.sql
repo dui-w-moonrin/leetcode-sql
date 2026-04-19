0128287cea04a94554feb7335c2e3dea3a66a97b
Watcharapong Moonrin <256491951+dui-w-moonrin@users.noreply.github.com>
2026-04-19 20:26:38 +0700

    add 1633

---
+++ 1633 percentage user attendanded.sql
@@ -0,0 +1,7 @@
+SELECT
+    r.contest_id,
+    ROUND( COUNT(DISTINCT u.user_id) * 100 / (SELECT COUNT(*) FROM Users) ,2) AS percentage
+FROM Users u
+JOIN Register r ON u.user_id = r.user_id
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id
